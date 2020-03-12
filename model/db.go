b4cf5b89892384c86e1cf0153d95d08bdc63f93f
metaclips <utimichael9@gmail.com>
2020-03-12 19:04:38 +0100

    implement user struct

---
+++ model/db.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var db *mongo.Client
+var ctx context.Context
+var cancel func()
+
+func init() {
+	var err error
+
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	db, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Ping mongo database if up
+	go func() {
+		for {
+			if err := db.Ping(ctx, readpref.Primary()); err != nil {
+				log.Fatalln(err)
+			}
+			time.Sleep(time.Second * 5)
+		}
+	}()
+}
+
+func ChangePassword() {
+
+}
+
+func AddToChat() {
+
+}
