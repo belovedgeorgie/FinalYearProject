38f8d12afc8810211b73c30d45d38c98dbf2c4ad
metaclips <utimichael9@gmail.com>
2020-03-13 10:22:41 +0100

    add spacing to struct tag key value pair

--- model/struct.go
+++ model/struct.go
@@ -12,35 +12,35 @@ const (
 )
 
 type StudentID struct {
-	Email        string `bson:"_id", json:"email"`
-	Name         string `bson:"name", json:"name"`
-	Age          int    `bson:"age", json:"age"`
-	ParentEmail  string `bson:"parentEmail", json:"parentEmail"`
-	ParentNumber string `bson:"parentNumber", json:"parentNumber"`
-	Password     []byte `bson:"password", json:"password"`
-	Faculty      string `bson:"faculty", json:"faculty"`
-	Department   string `bson:"department", json:"department"`
-	MatricNo     string `bson:"matricNo", json:"matricNo"`
+	Email        string `bson: "_id", json: "email"`
+	Name         string `bson: "name", json: "name"`
+	Age          int    `bson: "age", json: "age"`
+	ParentEmail  string `bson: "parentEmail", json: "parentEmail"`
+	ParentNumber string `bson: "parentNumber", json: "parentNumber"`
+	Password     []byte `bson: "password", json: "password"`
+	Faculty      string `bson: "faculty", json: "faculty"`
+	Department   string `bson: "department", json: "department"`
+	MatricNo     string `bson: "matricNo", json: "matricNo"`
 }
 
 type Chats struct {
-	UserEmail string  `bson:"_id", json:"email"`
-	Posts     []posts `bson:"posts", json:"posts"`
+	UserEmail string  `bson: "_id", json: "email"`
+	Posts     []posts `bson: "posts", json: "posts"`
 }
 
 type posts struct {
-	UserEmail string     `bson:"_id", json:"email"`
-	Messages  []messages `bson:"messages", json:"messages"`
+	UserEmail string     `bson: "_id", json: "email"`
+	Messages  []messages `bson: "messages", json: "messages"`
 }
 
 type messages struct {
-	Post  string `bson:"post", json:"post"`
-	Index int    `bson:"index", json:"index"`
-	Type  int    `bson:"type", json:"type"`
+	Post  string `bson: "post", json: "post"`
+	Index int    `bson: "index", json: "index"`
+	Type  int    `bson: "type", json: "type"`
 }
 
-// save files seperately and make sure they are distinct
+// FileType save files separately and make sure they are distinct
 type FileType struct {
-	Downloaded bool   `bson:"downloaded", json:"downloaded"`
-	Sha256     string `bson:"_id", json:"sha256"`
+	Downloaded bool   `bson: "downloaded", json: "downloaded"`
+	Sha256     string `bson: "_id", json: "sha256"`
 }
